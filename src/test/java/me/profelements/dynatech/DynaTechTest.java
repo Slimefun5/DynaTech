e8e0ac3437ea8471a220acc571ec37d57c633427
Intisy <finn@birich.de>
2026-04-26 22:34:21 +0200

    Add JUnit 5 and MockBukkit test scaffolding

---
+++ src/test/java/me/profelements/dynatech/DynaTechTest.java
@@ -0,0 +1,16 @@
+package me.profelements.dynatech;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Basic test scaffolding for DynaTech.
+ */
+class DynaTechTest {
+
+    @Test
+    void classLoads() {
+        assertNotNull(DynaTech.class);
+    }
+}
